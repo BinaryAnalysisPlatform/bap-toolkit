7b7744dc3edc01acafe1f97982271153e0ea520e
Oleg <forown@yandex.ru>
2020-02-26 09:50:54 -0500

    new with-no-return implementation

--- with-no-return/with_no_return.ml
+++ with-no-return/with_no_return.ml
@@ -1,154 +1,123 @@
 
+(**
+   This pass removes 'return' edge from all the calls to the non-return
+   functions.
+
+   Algorithm
+
+   We start from the initial set of non-return functions, such ones
+   that either have GNU noreturn attribute OR have an edge between
+   start and exit nodes in the Tid graph. For instance, the latter can
+   happen when a loop without exit exists in a graph of a subroutine.
+
+   Then, for each subroutine in the program, we remove such edges in the
+   Tid graph of the subroutine that correspond to calls to
+   non-return functions.
+
+   By doing so, we can introduce unreachable blocks. And to be sure
+   that they are trully unreachable, we need to prove that they can't
+   be reached by indirect calls. We add a synthetic node, and connect
+   each block with indirect call with this node and also connect this
+   node with the exit.
+
+   Now, the decision if the function is non-return one depends on
+   the reachability of the exit node from the start. If it can't
+   be reached then such subroutine is a non-return and we need to add
+   it in the set of nonreturn subroutines and apply algorithm
+   again to the all subroutines. *)
+
+
 open Core_kernel
 open Bap.Std
 open Graphlib.Std
 
 include Self ()
 
-module G = Graphlib.Make(Tid)(Unit)
+module G = Graphs.Tid
 
-type dest = [
-  | `Call of (tid option * tid option) (* target, return *)
-  | `Goto of tid
-]
-
-type graph = {
-  entry  : tid;
-  exit_  : tid;
-  no_ret : tid;
-  itself : G.t;
+type call_edge = {
+  src   : tid;
+  label : tid;
 }
 
 type subroutine = {
-  term  : sub term;
-  graph : graph option;
-  dests : dest list Tid.Map.t;
+  tid   : tid;
+  graph : G.t;
+  indirect : tid;
+  callees : call_edge list Tid.Map.t;
 }
 
-let return_of_call c =
-  match Call.return c with
-  | Some Direct t -> Some t
-  | _ -> None
+let is_resolved j =
+  List.filter_map ~f:ident [Jmp.dst j; Jmp.alt j] |>
+  List.exists ~f:(fun d -> Either.is_first (Jmp.resolve d))
 
-let dests_of_call c =
-  match Call.target c with
-  | Direct t -> Some (Some t, return_of_call c)
-  | Indirect _ ->
-    match return_of_call c with
-    | None -> None
-    | Some _ as ret -> Some (None, ret)
+let is_indirect j = not (is_resolved j)
 
-let dests_of_block b =
-  Seq.fold (Blk.elts b) ~init:[] ~f:(fun acc -> function
-      | `Def _ | `Phi _ -> acc
-      | `Jmp j ->
-        match Jmp.kind j with
-        | Int _ | Ret _ | Goto (Indirect _) -> acc
-        | Goto (Direct t) -> `Goto t :: acc
-        | Call c ->
-          match dests_of_call c with
-          | None -> acc
-          | Some d -> `Call d :: acc)
+let indirects_of_blk b =
+  Term.to_sequence jmp_t b |> Seq.filter ~f:is_indirect
 
-(** [make_no_return g caller callee] removes edge to a
-    non-return function with tid [callee] and connect
-    caller with an synthetic no-ret node *)
-let make_no_return g ~caller ~callee =
-  let itself =
-    Seq.fold (G.Node.outputs caller g.itself)
-      ~init:g.itself
-      ~f:(fun g e ->
-          if Tid.(G.Edge.dst e = callee)
-          then G.Edge.remove e g
-          else g) in
-  let itself = G.Edge.(insert (create caller g.no_ret ()) itself) in
-  { g with itself }
+let connect_indirects g node blk =
+  Seq.fold (indirects_of_blk blk) ~init:g
+    ~f:(fun g j ->
+        let e = G.Edge.create (Term.tid blk) node (Term.tid j) in
+        G.Edge.insert e g)
 
-let find_callees dests  =
-  List.filter_map dests ~f:(function
-      | `Call (Some x,_) -> Some x
-      | _ -> None)
+let calls_of_blk calls b =
+  Term.to_sequence jmp_t b |>
+  Seq.fold ~init:calls ~f:(fun acc j ->
+      match Jmp.alt j with
+      | None -> acc
+      | Some callee -> match Jmp.resolve callee with
+        | First callee ->
+          Map.add_multi acc callee { src = Term.tid b; label = Term.tid j; }
+        | _ -> acc)
 
-(** [update norets sub] updates sub's graph, so
-    any edge to function in [norets] will be replaced
-    with an edge to synthetic no-return node *)
-let update no_rets sub =
-  match sub.graph with
-  | None -> sub
-  | Some g ->
-    let g =
-      Map.fold sub.dests ~init:g
-        ~f:(fun ~key:caller ~data:dests init ->
-            List.fold (find_callees dests)
-              ~init ~f:(fun g callee ->
-                  if Set.mem no_rets callee then
-                    make_no_return g caller callee
-                  else g)) in
-    {sub with graph = Some g;}
-
-let to_graph start dests =
-  let insert_dest g node = function
-    | `Goto tid ->
-      let g = G.Edge.(insert (create node tid ()) g) in
-      g, [tid]
-    | `Call (tar,ret) ->
-      match tar,ret with
-      | Some tar, Some ret ->
-        let g = G.Edge.(insert (create node tar ()) g) in
-        let g = G.Edge.(insert (create tar ret ()) g)  in
-        g, [tar; ret]
-      | Some tar, None ->
-        G.Edge.(insert (create node tar ()) g), [tar]
-      | None, None -> g, []
-      | None, Some ret ->
-        let tar = Tid.create () in
-        let g = G.Edge.(insert (create node tar ()) g) in
-        let g = G.Edge.(insert (create tar ret ()) g) in
-        g, [ret] in
-  let rec loop g node =
-    let g = G.Node.insert node g in
-    match Map.find dests node with
-    | None -> g
-    | Some dsts ->
-      let g',dsts =
-        List.fold dsts ~init:(g,[])
-          ~f:(fun (g,dsts) dst ->
-              let g, dsts' = insert_dest g node dst in
-              g, dsts' @ dsts) in
-      let dsts = List.filter ~f:(fun n -> not (G.Node.mem n g)) dsts in
-      match dsts with
-      | [] -> g'
-      | dsts -> List.fold dsts ~init:g' ~f:loop in
-  loop G.empty start
-
-let collect_destinations sub =
+let callees_of_sub sub =
   Term.to_sequence blk_t sub |>
-  Seq.fold ~init:(Map.empty (module Tid))
-    ~f:(fun (dests) b ->
-        Map.set dests (Term.tid b) (dests_of_block b))
+  Seq.fold ~init:(Map.empty (module Tid)) ~f:calls_of_blk
 
-let of_sub s =
-  let blks = Term.to_sequence blk_t s in
-  let dests = collect_destinations s in
-  match Seq.hd blks with
-  | None -> {term=s; graph=None; dests}
-  | Some hd ->
-    let no_ret = Tid.create () in
-    let exit_ = Tid.create () in
-    let g = to_graph (Term.tid hd) dests in
-    let entry = Term.tid hd in
-    let g =
-      G.nodes g |>
-      Seq.fold ~init:g ~f:(fun g n ->
-          if G.Node.degree ~dir:`Out n g = 0 && Tid.(n <> exit_) then
-            G.Edge.(insert (create n exit_ ()) g)
-          else g) in
-    let g = G.Edge.(insert (create no_ret exit_ ()) g) in
-    let graph = {entry;exit_;no_ret;itself=g} in
-    {term = s; graph = Some graph; dests}
+let of_sub sub =
+  let blks = Term.to_sequence blk_t sub in
+  if Seq.is_empty blks then None
+  else
+    let indirect = Tid.create () in
+    let graph = Sub.to_graph sub in
+    let graph = G.Edge.(insert (create indirect G.exit indirect) graph) in
+    let graph =
+      Seq.fold blks ~init:graph
+        ~f:(fun g b -> connect_indirects g indirect b) in
+    let callees = callees_of_sub sub in
+    Some {tid = Term.tid sub; graph; indirect; callees}
 
+let prune_edge_to_nonreturn g {src; label} =
+  G.Node.outputs src g |>
+  Seq.fold ~init:g ~f:(fun g e ->
+      if Tid.equal (G.Edge.label e) label
+      then G.Edge.remove e g
+      else g)
 
-let map no_returns sub =
+let update s no_ret =
+  let is_unreachable b g = G.Node.degree ~dir:`Out b g = 0 in
+  match Map.find s.callees no_ret with
+  | None -> s
+  | Some edges -> {
+      s with
+      graph =
+        List.fold edges ~init:s.graph
+          ~f:(fun g edge ->
+              let dsts =
+                G.Node.outputs edge.src g |> Seq.map ~f:G.Edge.dst in
+              let g = prune_edge_to_nonreturn g edge in
+              Seq.fold dsts ~init:g
+                ~f:(fun g blk ->
+                    if is_unreachable blk g then
+                      G.Edge.(insert (create blk s.indirect s.indirect) g)
+                    else g))
+    }
+
+let update s no_rets = Set.fold no_rets ~init:s ~f:update
+
+let apply no_returns prog =
   (object
     inherit Term.mapper
 
@@ -162,50 +131,45 @@ let map no_returns sub =
           if Set.mem no_returns tid then
             Jmp.with_kind j (Call (Call.with_noreturn c))
           else j
-  end)#map_sub sub
+  end)#run prog
+
+let is_known_noreturn s g =
+  Term.has_attr s Sub.noreturn ||
+  G.Node.has_edge G.start G.exit g
 
 let of_prog prog =
   Term.to_sequence sub_t prog |>
   Seq.fold ~init:([],Set.empty (module Tid))
-    ~f:(fun (subs,no_rets) s ->
-        let no_rets =
-          match Term.get_attr s Sub.noreturn with
-          | None -> no_rets
-          | Some _ -> Set.add no_rets (Term.tid s) in
-        of_sub s :: subs, no_rets)
+    ~f:(fun (subs,no_rets) s -> match of_sub s with
+        | None -> subs, no_rets
+        | Some s' ->
+          let no_rets =
+            if is_known_noreturn s s'.graph then
+              Set.add no_rets s'.tid
+            else no_rets in
+          s' :: subs, no_rets)
 
-(** [is_no_return sub] returns true if the no-ret node
-    postdominate sub's entry node *)
 let is_no_return sub =
-  match sub.graph with
-  | None -> false
-  | Some g ->
-    let doms = Graphlib.dominators (module G) ~rev:true g.itself g.exit_ in
-    Tree.is_descendant_of doms ~parent:g.no_ret g.entry
+  not (Graphlib.is_reachable (module G) sub.graph G.start G.exit)
 
 let run prog =
-  let run_prog subs no_rets =
+  let update_prog subs no_rets =
     List.fold subs ~init:([],no_rets) ~f:(fun (subs, no_rets) s ->
-        if Set.mem no_rets (Term.tid s.term) then s :: subs, no_rets
-        else
-          let s = update no_rets s in
+        let s = update s no_rets in
+        let no_rets =
           if is_no_return s then
-            s :: subs, Set.add no_rets (Term.tid s.term)
-          else s :: subs,no_rets) in
+            Set.add no_rets s.tid
+          else no_rets in
+        s :: subs, no_rets) in
   let rec loop subs no_rets =
-    let subs,no_rets' = run_prog subs no_rets in
-    if Set.is_empty (Set.diff no_rets' no_rets) then subs,no_rets
+    let subs,no_rets' = update_prog subs no_rets in
+    if Set.is_empty (Set.diff no_rets' no_rets) then no_rets
     else loop subs no_rets' in
   let subs,no_rets = of_prog prog in
-  let subs,no_rets = loop subs no_rets in
-  let bld = Program.Builder.create () in
-  List.iter subs ~f:(fun s ->
-      let s = map no_rets s.term in
-      Program.Builder.add_sub bld s);
-  Program.Builder.result bld
+  let no_rets = loop subs no_rets in
+  apply no_rets prog
 
-let main p =
-  Project.with_program p (run (Project.program p))
+let main p = Project.with_program p (run (Project.program p))
 
 let () =
   Config.when_ready (fun _ -> Project.register_pass ~deps:["api"] main)
